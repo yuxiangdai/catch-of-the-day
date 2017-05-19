c0620564443ec3a544e5c7eec5b815773097a91d
Yuxiang Dai <yuxiang.dai43@gmail.com>
2017-05-19 10:07:18 -0400

    new files

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -9,7 +9,9 @@ class StorePicker extends React.Component {
 
 	goToStore(event) {
 		event.preventDefault();	
-		console.log(this.storeInput);
+		const storeID = this.storeInput.value;
+		console.log(`Going to ${storeID}`)
+		this.context.router.transitionTo(`/store/${storeID}`);
 	}
 	render(){
 		return (
@@ -22,6 +24,10 @@ class StorePicker extends React.Component {
 			</form>
 		)
 	}
+}
+
+StorePicker.contextTypes = {
+	router: React.PropTypes.object
 }
 
 export default StorePicker;
