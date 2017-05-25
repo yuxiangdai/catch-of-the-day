80253379df453c60059253c6201f4e85520c1e18
Yuxiang Dai <yuxiang.dai43@gmail.com>
2017-05-24 22:03:36 -0400

    loading state onClick

--- src/components/App.js
+++ src/components/App.js
@@ -2,17 +2,54 @@ import React from 'react';
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
+import Fish from './Fish';
+import sampleFishes from '../sample-fishes';
 
 class App extends React.Component
 {
+	constructor(){
+		super();
+
+		this.addFish = this.addFish.bind(this);
+		this.loadSamples = this.loadSamples.bind(this);
+		// getInitialState
+		this.state = {
+			fishes: {},
+			order: {}
+		};
+	}
+
+	addFish(fish){
+		//update our state
+		const fishes = {...this.state.fishes}; //... takes copy of existing state
+		//add in our new fish 
+		const timestamp = Date.now();
+		fishes[`fish-${timestamp}`] = fish;
+		//set state
+		this.setState({ fishes })
+	}
+
+	loadSamples(){	
+		this.setState({
+			fishes: sampleFishes
+		});
+	}
+
 	render(){
 		return(
 			<div className="catch-of-the-day">
 				<div className="menu">
 					<Header tagline="Fresh Seafood Market"/>
+					<ul className="list-of-fishes">
+						{
+							Object
+							.keys(this.state.fishes)
+							.map(key => <Fish />)
+						}
+					</ul>
 				</div>
 				<Order/>
-				<Inventory />
+				<Inventory addFish={this.addFish}  loadSamples={this.loadSamples}/>
 			</div>
 		)
 	}
--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -3,12 +3,12 @@ import AddFishForm from './AddFishForm'
 
 class Inventory extends React.Component
 {
-	render()
-	{
+	render() {
 		return(
 		    <div>
 		    	<p>Inventory</p>
-		    	<AddFishForm />
+		    	<AddFishForm addFish={this.props.addFish}/>
+		    	<button onClick={this.props.loadSamples}>Load Sample Fishes</button>
 		    </div>
 			
 		)
